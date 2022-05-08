387987185e5517faa39e7060009883f91b5ad4d7
Sarim Asif <sarim.pro@gmail.com>
2022-05-09 00:05:56 +0200

    update deploy script & add vol mount

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -1,11 +1,25 @@
+mkdir -p cluster-volume
+
 kind create cluster --name local --config=$(dirname "$0")/kind/cluster-config.yaml
 kubectl cluster-info --context kind-local
 
-println("Setting up Nginx Ingress Controller...")
+printf "Setting up Nginx Ingress Controller..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-println("Setting up Tekton...")
+printf "Setting up Tekton..."
 kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/webhooks.yaml
+
+printf "Setting up Tekton Dashboard..."
 kubectl apply -f https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
-kubectl apply -f resources/tekton-dashboard-ing.yaml
+
+until [ $(kubectl get pods -l app.kubernetes.io/component=controller -n ingress-nginx -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == "True" ]; do
+    echo "Waiting for Nginx Ingress Controller to be ready..."
+    sleep 15
+done
+
+if kubectl get po -n ingress-nginx -o wide | grep controller | grep -q 'Running'; then
+   kubectl apply -f resources/tekton-dashboard-ing.yaml
+fi
+
